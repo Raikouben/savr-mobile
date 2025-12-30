3101a80ad720e9a197b2194c735f684264bb9453
Raikouben <maharjananish2005@gmail.com>
2025-12-30 14:54:14 +0000

    configured login and signout using clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { Slot } from "expo-router";
 import SafeView from "../components/SafeView";
 
 export default function RootLayout() {
   return (
-    <SafeView>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeView>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeView>
+        <Slot />
+      </SafeView>
+    </ClerkProvider>
   );
 }
