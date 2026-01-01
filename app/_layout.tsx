bb4fd57950a2cf7105c6ae2bf5580af917d11491
Raikouben <maharjananish2005@gmail.com>
2026-01-01 13:20:27 +0000

    Added splash screen back so app waits for state load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,32 @@
-import { ClerkProvider } from "@clerk/clerk-expo";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
 import SafeView from "../components/SafeView";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+function RootLayoutNav() {
+  const { isLoaded } = useAuth();
+
+  useEffect(() => {
+    if (isLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  return (
+    <SafeView>
+      <Slot />
+    </SafeView>
+  );
+}
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <SafeView>
-        <Slot />
-      </SafeView>
+      <RootLayoutNav />
     </ClerkProvider>
   );
 }
